641fda02b6ef9da8a29b2a607c6eaec32a164fdd
marianneji <marianneji@gmail.com>
2020-03-05 10:41:32 +0100

    create a extension to SKNode to scale the birds to fit the scenes

---
+++ BirdyBirds/Extension/SKNodes+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  SKNodes+Extensions.swift
+//  BirdyBirds
+//
+//  Created by Graphic Influence on 05/03/2020.
+//  Copyright © 2020 marianne massé. All rights reserved.
+//
+
+import SpriteKit
+
+extension SKNode {
+
+    func aspectScale(to size: CGSize, width: Bool, multiplier: CGFloat) {
+        let scale = width ? (size.width * multiplier) / self.frame.size.width : (size.height * multiplier) / self.frame.size.height
+        self.setScale(scale)
+    }
+}

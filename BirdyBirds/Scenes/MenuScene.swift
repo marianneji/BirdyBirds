0679df378a76ecfeaedc230c7f24299e8f63328f
marianneji <marianneji@gmail.com>
2020-03-06 10:05:32 +0100

    add background to menuScene

--- BirdyBirds/Scenes/MenuScene.swift
+++ BirdyBirds/Scenes/MenuScene.swift
@@ -17,9 +17,17 @@ class MenuScene: SKScene {
     }
 
     func setupMenu() {
+
+        let background = SKSpriteNode(imageNamed: "menuBackground")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.aspectScale(to: frame.size, width: true, multiplier: 1)
+        background.zPosition = ZPosition.background
+        addChild(background)
+
         let button = SpriteKitButton(defaultButtonImage: "playButton", action: goTOLevelScene, index: 0)
-        button.position = CGPoint(x: frame.midX, y: frame.midY)
+        button.position = CGPoint(x: frame.midX, y: frame.midY * 0.8)
         button.aspectScale(to: frame.size, width: false, multiplier: 0.2)
+        button.zPosition = ZPosition.hudLabel
         addChild(button)
     }
 

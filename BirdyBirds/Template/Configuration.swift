e727e12ea592348306053904da3acc4a83c4bca4
marianneji <marianneji@gmail.com>
2020-03-06 10:06:28 +0100

    add bird in Position and enemy for PhysicsCategories

--- BirdyBirds/Template/Configuration.swift
+++ BirdyBirds/Template/Configuration.swift
@@ -15,6 +15,7 @@ struct Levels {
 struct ZPosition {
     static let background: CGFloat = 0
     static let obstacles: CGFloat = 1
+    static let birds: CGFloat = 2
     static let hudBackground: CGFloat = 10
     static let hudLabel: CGFloat = 11
 }
@@ -25,6 +26,7 @@ struct PhysicsCategories {
     static let edge: UInt32 = 0x1
     static let bird: UInt32 = 0x1 << 1
     static let block: UInt32 = 0x1 << 2
+    static let enemy: UInt32 = 0x1 << 3
 }
 
 extension CGPoint {
